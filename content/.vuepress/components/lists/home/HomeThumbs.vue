59c4440a48728a40723192b6577b99be04930144
Dave Stewart <dave@davestewart.co.uk>
2022-02-28 01:54:05 +0000

    Tweak home thumbs copy

--- content/.vuepress/components/lists/home/HomeThumbs.vue
+++ content/.vuepress/components/lists/home/HomeThumbs.vue
@@ -2,7 +2,7 @@
   <div class="homeThumbs">
     <p>
       Here's a <a href="#recent" :class="{ active: !options.random }" @click.prevent="setRecent">recent</a>
-      (or <a href="#random" :class="{ active: options.random }" @click.prevent="setRandom">random!</a>)
+      / <a href="#random" :class="{ active: options.random }" @click.prevent="setRandom">random</a>
       sample of what I do:</p>
     <ThumbnailWall :pages="pages"/>
   </div>
