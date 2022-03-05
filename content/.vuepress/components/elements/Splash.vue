84e484289b77ccc22abedcda32d1ea46e8958316
Dave Stewart <dave@davestewart.co.uk>
2022-03-05 21:37:51 +0000

    Tweak splash animation

--- content/.vuepress/components/elements/Splash.vue
+++ content/.vuepress/components/elements/Splash.vue
@@ -8,7 +8,7 @@
     <g id="logo"
        @mouseenter="onMouseEnter"
        @mouseleave="onMouseLeave"
-       @mousemove="animate"
+       @mousemove="onMouseMove"
        transform="translate(59.078 131.608)" fill="#EA4848" fill-rule="nonzero">
       <path id="mouth" d="M96.232 136.84c9.037 0 17.262-1.706 24.678-5.118 7.415-3.413 13.753-8.171 19.016-14.276 5.262-6.105 9.342-13.337 12.239-21.695 2.896-8.36 4.345-17.485 4.345-27.378 0-9.83-1.449-18.925-4.345-27.283-2.897-8.36-6.977-15.575-12.24-21.649-5.262-6.073-11.6-10.832-19.015-14.275C113.494 1.722 105.269 0 96.232 0H58.917v136.84h37.315Zm-10.206-24.419V24.42h10.926c5.156 0 9.754 1.017 13.794 3.052 4.04 2.035 7.455 4.947 10.246 8.735 2.79 3.788 4.93 8.406 6.418 13.853 1.488 5.447 2.233 11.552 2.233 18.314 0 6.825-.745 12.96-2.233 18.408s-3.628 10.065-6.418 13.853c-2.791 3.788-6.206 6.7-10.246 8.735-4.04 2.035-8.638 3.052-13.794 3.052H86.026Z" />
       <path id="eyes" d="M18.341 55.515c2.57 0 4.967-.464 7.19-1.394a18.243 18.243 0 0 0 5.836-3.861c1.668-1.645 2.97-3.593 3.908-5.846.938-2.252 1.407-4.665 1.407-7.24 0-2.574-.469-4.97-1.407-7.186a17.771 17.771 0 0 0-3.908-5.792c-1.667-1.644-3.612-2.95-5.835-3.915-2.224-.965-4.62-1.448-7.191-1.448-2.64 0-5.072.483-7.295 1.448-2.223.966-4.15 2.27-5.783 3.915s-2.918 3.575-3.856 5.792C.469 32.205 0 34.6 0 37.174c0 2.575.469 4.988 1.407 7.24.938 2.253 2.223 4.201 3.856 5.846 1.632 1.644 3.56 2.931 5.783 3.861 2.223.93 4.655 1.394 7.295 1.394ZM18.341 120.865c2.57 0 4.967-.465 7.19-1.394a18.243 18.243 0 0 0 5.836-3.862c1.668-1.644 2.97-3.593 3.908-5.845.938-2.253 1.407-4.666 1.407-7.24s-.469-4.97-1.407-7.186a17.771 17.771 0 0 0-3.908-5.792c-1.667-1.645-3.612-2.95-5.835-3.915-2.224-.965-4.62-1.448-7.191-1.448-2.64 0-5.072.483-7.295 1.448s-4.15 2.27-5.783 3.915C3.63 91.19 2.345 93.12 1.407 95.338.469 97.554 0 99.95 0 102.524s.469 4.987 1.407 7.24c.938 2.252 2.223 4.2 3.856 5.845 1.632 1.645 3.56 2.932 5.783 3.862 2.223.93 4.655 1.394 7.295 1.394Z" />
@@ -27,18 +27,39 @@
         <path id="o" d="M314.524 43.261c22.592 0 40.537 17.465 40.537 39.896 0 22.272-17.945 39.897-40.537 39.897s-40.537-17.625-40.537-39.897c0-22.431 17.945-39.896 40.537-39.896Zm0 17.945c-12.978 0-20.99 10.095-20.99 21.951 0 11.857 8.012 21.951 20.99 21.951 12.979 0 20.99-10.094 20.99-21.95 0-11.857-8.011-21.952-20.99-21.952Z"/>
       </g>
       <g id="dots">
-        <path id="dot-1" d="M380.537 123.054c7.05 0 12.658-5.608 12.658-12.658s-5.608-12.658-12.658-12.658-12.657 5.608-12.657 12.658 5.608 12.658 12.657 12.658Z"/>
-        <path id="dot-2" d="M419.567 123.054c7.05 0 12.658-5.608 12.658-12.658s-5.608-12.658-12.658-12.658-12.658 5.608-12.658 12.658 5.608 12.658 12.658 12.658Z"/>
-        <path id="dot-3" d="M458.596 123.054c7.05 0 12.658-5.608 12.658-12.658s-5.608-12.658-12.658-12.658-12.658 5.608-12.658 12.658 5.608 12.658 12.658 12.658Z"/>
+        <path id="dot-1" @click="onDotClick" @animationend="onDotAnimationEnd" d="M380.537 123.054c7.05 0 12.658-5.608 12.658-12.658s-5.608-12.658-12.658-12.658-12.657 5.608-12.657 12.658 5.608 12.658 12.657 12.658Z"/>
+        <path id="dot-2" @click="onDotClick" @animationend="onDotAnimationEnd" d="M419.567 123.054c7.05 0 12.658-5.608 12.658-12.658s-5.608-12.658-12.658-12.658-12.658 5.608-12.658 12.658 5.608 12.658 12.658 12.658Z"/>
+        <path id="dot-3" @click="onDotClick" @animationend="onDotAnimationEnd" d="M458.596 123.054c7.05 0 12.658-5.608 12.658-12.658s-5.608-12.658-12.658-12.658-12.658 5.608-12.658 12.658 5.608 12.658 12.658 12.658Z"/>
       </g>
     </g>
   </svg>
 </template>
 
 <script>
-import { debounce } from 'throttle-debounce'
+import { throttle } from 'throttle-debounce'
+
+function pos (event) {
+  const x = event.clientX
+  const y = event.clientY
+  return { x, y }
+}
+
+function dist (a, b) {
+  const dx = a.x - b.x
+  const dy = a.y - b.y
+  return Math.sqrt(dx * dx + dy * dy)
+}
 
 export default {
+  data () {
+    return {
+      pos: {
+        x: 0,
+        y: 0,
+      }
+    }
+  },
+
   mounted () {
     const animCount = parseInt(document.body.getAttribute('data-animCount') || 0)
     if (animCount < 3) {
@@ -48,7 +69,7 @@ export default {
   },
 
   methods: {
-    animate (delay = 300) {
+    animate (delay = 0) {
       this.stop()
       this.timeoutId = setTimeout(() => {
         this.$el.classList.add('bounce')
@@ -59,13 +80,25 @@ export default {
       clearTimeout(this.timeoutId)
     },
 
-    onMouseEnter () {
-      this.animate(0)
+    onDotClick (event) {
+      event.preventDefault()
+      event.stopImmediatePropagation()
+      event.currentTarget.classList.add('bounce')
     },
 
-    onMouseMove () {
-      this.animate(0)
+    onMouseEnter (event) {
+      this.pos = pos(event)
+      this.animate()
     },
+
+    onMouseMove: throttle(100, function (event) {
+      const p = pos(event)
+      const d = dist(this.pos, p)
+      this.pos = p
+      if (d > 20) {
+        this.animate()
+      }
+    }),
 
     onMouseLeave () {
       this.stop()
@@ -77,6 +110,14 @@ export default {
     onAnimationEnd (event) {
       if (event.target.id === 'dot-3') {
         this.$el.classList.remove('bounce')
+      }
+    },
+
+    onDotAnimationEnd (event) {
+      const target = event.target
+      const classList = target.classList
+      if (target.id.startsWith('dot') && classList.contains('bounce')) {
+        classList.remove('bounce')
       }
     }
   }
@@ -105,6 +146,12 @@ export default {
     #dot-1 { animation-delay: .0s; }
     #dot-2 { animation-delay: .1s; }
     #dot-3 { animation-delay: .17s; }
+  }
+
+  #dot-1.bounce,
+  #dot-2.bounce,
+  #dot-3.bounce {
+    animation: bounce-3 1.1s;
   }
 }
 
